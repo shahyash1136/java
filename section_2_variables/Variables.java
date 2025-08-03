6d463e677b2e57c0acc1883f64bc26cf1c319340
Yash Shah <43175218+shahyash1136@users.noreply.github.com>
2025-08-03 21:30:55 +0530

    Completed variables section

---
+++ section_2_variables/Variables.java
@@ -0,0 +1,11 @@
+// Removed package declaration to match the expected package
+
+class Variables {
+    public static void main(String[] args) {
+        int passengers = 5;
+        System.out.println(passengers);
+
+        int busTickets = passengers;
+        System.out.println(busTickets);
+    }
+}
---
+++ section_2_variables/WholeNumbers.java
@@ -0,0 +1,8 @@
+public class WholeNumbers {
+    public static void main(String[] args) {
+        long globalPopulation = 8_000_000_000L;
+        System.out.println("The global population is " + globalPopulation);
+        long dialyGoogleSearch = 8_500_000_000L;
+        System.out.println("Daily, there are " + dialyGoogleSearch + " google searches");
+    }
+}
