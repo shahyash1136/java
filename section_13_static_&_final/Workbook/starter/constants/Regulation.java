f3a9bca8c4980ab4187a77a95d21b09300680088
Yash Shah <43175218+shahyash1136@users.noreply.github.com>
2025-09-06 00:13:09 +0530

    completed static and final section

---
+++ section_13_static_&_final/Workbook/starter/constants/Regulation.java
@@ -0,0 +1,15 @@
+package constants;
+
+public class Regulation {
+
+    // constants go here
+    public static final int COURT_LENGTH = 0;
+    public static final int RIM_HEIGHT = 0;
+    public static final int THREE_POINT_DISTANCE = 0;
+    public static final int BEYOND_THREE_POINT_ARC = 0;
+    public static final int INSIDE_THREE_POINT_ARC = 0;
+    public static final int FREE_THROW = 0;
+    public static final int QUARTER_LENGTH = 0;
+    public static final int POSSESSION_TIME = 0;
+
+}
